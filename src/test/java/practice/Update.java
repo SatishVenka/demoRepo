e47cf6fdd506d286ce3819aa9bcec132ca02f4c3
satishVenka <satishvenket26@gmail.com>
2023-08-18 16:50:49 +0530

    commit and push

---
+++ src/test/java/practice/Update.java
@@ -0,0 +1,12 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class Update {
+@Test
+	public void updatefile() {
+		// TODO Auto-generated constructor stub
+	System.out.println("----update--");
+	}
+
+}
---
+++ src/test/java/practice/Visual.java
@@ -0,0 +1,12 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class Visual {
+@Test
+	public void visualtest() {
+		// TODO Auto-generated constructor stub
+	System.out.println("---visual--");
+	}
+
+}
